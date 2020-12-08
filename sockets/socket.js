7533c1fb27dad483f262eee8c5d9aae1ee79abb1
Aldair Carrillo <aldacdh904@gmail.com>
2020-12-07 19:08:56 -0700

    last version

--- sockets/socket.js
+++ sockets/socket.js
@@ -26,26 +26,26 @@ io.on('connection', client => {
     io.emit('mensaje', { admin: 'Nuevo mensaje EMitido'});
    });
 
-   client.on('emitir-mensaje', (payload) => {
-     //console.log(payload);
-    //io.emit('nuevo-mensaje', payload); ///emite a todos!
-    client.broadcast.emit('nuevo-mensaje', payload); /// emite a todos menos el que lo emitio
-   });
+  //  client.on('emitir-mensaje', (payload) => {
+  //    //console.log(payload);
+  //   //io.emit('nuevo-mensaje', payload); ///emite a todos!
+  //   client.broadcast.emit('nuevo-mensaje', payload); /// emite a todos menos el que lo emitio
+  //  });
 
    client.on('vote-band', (payload) => {
     bands.voteBand(payload.id);
-    io.emit('active-bands'. bands.getBands());
+    io.emit('active-bands', bands.getBands());
    });
 
    client.on('add-band', (payload) => {
      const newBand = new Band(payload.name);
     bands.addBand(newBand);
-    io.emit('active-bands'. bands.getBands());
+    io.emit('active-bands', bands.getBands());
    });
 
    client.on('delete-band', (payload) => {
    bands.deleteBand(payload.id);
-   io.emit('active-bands'. bands.getBands());
+   io.emit('active-bands', bands.getBands());
   });
 
 });
