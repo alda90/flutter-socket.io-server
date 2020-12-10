5bf54ba24b63164849fafe02c41800e2e26b2c00
Aldair Carrillo <aldacdh904@gmail.com>
2020-12-10 16:01:32 -0700

    Update index

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
+// const app = require('express')()
+// const server = require('http').createServer(app)
+// const Band = require('./models/band')
+// const Bands = require('./models/bands')
+
+
+// //const path = require('path');
+// //require('dotenv').config();
+
+
+// const bands = new Bands();
+// bands.addBand(new Band('Queen'));
+// bands.addBand(new Band('Heróes del Silencio'));
+// bands.addBand(new Band('The Clash'));
+// bands.addBand(new Band('The Cure'));
+
+// // App de Express
+// //const app = express();
+
+
+// // Node Server
+
+
+
+// //module.exports.io = require('socket.io')(server);
+// //require('./sockets/socket')
+// /// Path Publico
+// // const publicPath = path.resolve( __dirname, 'public');
+// // app.use(express.static(publicPath));
+
+// app.get('/', (req, res) => {
+//     res.send("Node Server is running. Yay!!")
+// })
+
+// const socketio = require('socket.io')(server)
+
+// socketio.on("connection", (userSocket) => {
+//     // userSocket.on("send_message", (data) => {
+//     //     userSocket.broadcast.emit("receive_message", data)
+//     // })
+
+//     userSocket.emit('active-bands', bands.getBands())
+// })
+
+
+
+// server.listen(process.env.PORT);
+
 const app = require('express')()
-const server = require('http').createServer(app)
-//const path = require('path');
-//require('dotenv').config();
+const http = require('http').createServer(app)
 const Band = require('./models/band')
 const Bands = require('./models/bands')
 
@@ -11,34 +57,21 @@ bands.addBand(new Band('Heróes del Silencio'));
 bands.addBand(new Band('The Clash'));
 bands.addBand(new Band('The Cure'));
 
-// App de Express
-//const app = express();
 
-
-// Node Server
-
-
-
-//module.exports.io = require('socket.io')(server);
-//require('./sockets/socket')
-/// Path Publico
-// const publicPath = path.resolve( __dirname, 'public');
-// app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
     res.send("Node Server is running. Yay!!")
 })
 
-const socketio = require('socket.io')(server)
+//Socket Logic
+const socketio = require('socket.io')(http)
 
 socketio.on("connection", (userSocket) => {
-    // userSocket.on("send_message", (data) => {
-    //     userSocket.broadcast.emit("receive_message", data)
-    // })
+    userSocket.on("send_message", (data) => {
+        userSocket.broadcast.emit("receive_message", data)
+    })
 
     userSocket.emit('active-bands', bands.getBands())
 })
 
-
-
-server.listen(process.env.PORT);
+http.listen(process.env.PORT)
