5d8d7c8d65a6250824fe3986bdaff6d914757e6c
Aldair Carrillo <aldacdh904@gmail.com>
2020-12-10 14:49:19 -0700

    socket

--- index.js
+++ index.js
@@ -8,11 +8,23 @@ const app = express();
 
 // Node Server
 const server = require('http').createServer(app);
-module.exports.io = require('socket.io')(server);
-require('./sockets/socket')
+
+
+//module.exports.io = require('socket.io')(server);
+//require('./sockets/socket')
 /// Path Publico
 // const publicPath = path.resolve( __dirname, 'public');
 // app.use(express.static(publicPath));
+
+const socketio = require('socket.io')(http)
+
+socketio.on("connection", (userSocket) => {
+    // userSocket.on("send_message", (data) => {
+    //     userSocket.broadcast.emit("receive_message", data)
+    // })
+
+    // userSocket.emit('active-bands', bands.getBands())
+})
 
 app.get('/', (req, res) => {
     res.send("Node Server is running. Yay!!")
