46aee69aba56b669a8e0fc0956ee08ac049c18dd
Aldair Carrillo <aldacdh904@gmail.com>
2020-12-10 14:46:15 -0700

    node server is running

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket')
 /// Path Publico
-const publicPath = path.resolve( __dirname, 'public');
-app.use(express.static(publicPath));
+// const publicPath = path.resolve( __dirname, 'public');
+// app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
 
 server.listen(process.env.PORT);
